ba55762b2cb49e167ae6c335cf7da379b39ebe2b
tssnainital-byte <tssnainital@gmail.com>
2026-03-24 16:57:31 +0530

    Add Board component with university portal layout

---
+++ Board.jsx
@@ -0,0 +1,419 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Board = () => {
+  return (
+    <>
+      
+  
+    
+      <div className="container" style={{ height: 'calc(100vh - 140px)', overflow: 'auto' }}>
+    
+    <img src="https://www.pngall.com/wp-content/uploads/4/Border-PNG-File.png" height="280px" width="290px"/>
+    <span  class="text-center new" style={{marginLeft:"400px" , marginTop:"-40px"}}> <h2>  XYZ  UNIVERSITY  </h2></span>
+    
+     <br></br><br></br><br></br><br></br>
+     
+     
+     
+      <div id="liveAlertPlaceholder"><div><div class="alert alert-success alert-dismissible" role="alert">   <div>  <h5 style={{color:"red"}}>Hello!  "Welcome in the university portal" </h5></div>   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div></div><div><div class="alert alert-success alert-dismissible" role="alert">   <div>Thankyou for reading our messagee!</div>   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div></div></div>
+ 
+    
+  
+    
+     <br></br>
+     
+     
+    
+     
+     <span class="marquee">  <span> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px" , paddingTop:"0px"}} >New</span> </button> About convocation 2026 </span>    <span style={{ marginRight: '80px' , marginLeft:"25px" }}> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px"}} >New</span> </button> earth day program </span>  <span style={{ marginRight: '50px' }}> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px"}} >New</span> </button> regarding new ug registration</span>  <span style={{ marginRight: '50px' }}> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px"}} >New</span> </button> regarding new mess policy </span>  <span style={{marginLeft:"50px"}}> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px"}} >New</span> </button> liabrary news</span>    <span> <button class="btn btn-danger news"> <span style={{marginTop:"-34px" , marginLeft:"-5px" , paddingTop:"0px"}} >New</span> </button> Regarding hostel facility</span>    </span>
+
+     
+     
+     
+     
+     
+     
+     <br></br>
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    
+    
+    <div class="container-lg">
+      
+  <img   src=" images/meanstack (2).jpeg "     />  
+      
+      
+     
+     
+    
+     <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://educationpost.in/_next/image?url=https%3A%2F%2Fapi.educationpost.in%2Fs3-images%2F1751901196275-1..jpg&w=3840&q=75" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/067/054/767/small/emotional-multiracial-students-having-graduation-party-at-university-or-college-campus-takings-on-mobile-phone-happy-international-group-of-graduates-posing-together-with-diplomas-photo.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://indiaeducation.net/wp-content/uploads/2023/08/Untitled-design-1-1024x768.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+     <div class="carousel-item">
+      <img src="https://i.pinimg.com/564x/e9/8c/c7/e98cc7acd077288f4e016a917164e643.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+
+<br></br><br></br>
+
+<br></br><br></br>
+
+  <div class="container">
+ <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     
+		<div class="container-xxl amiti">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://lh5.googleusercontent.com/proxy/k8CSJ4dTRp1OtqYT3a9HQ2c2QxFrI7FxIrjtusGcTNaj-b3lVtQ7F4vBeA2zgXsqqD8TpYeYl8f9TqpyRTCM1Yn33I4LezSnpBUSgwdzRjdqQs9LGK2r-O_O9JE4yUWAub9yui25sVGL" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz1P_KfPq_rmdp5RSVZwRbKNhPhupZkNhKvw&s" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+      <img src="https://lh5.googleusercontent.com/proxy/JEGHQ-FXKHZMDhMtoE-qjSt4nNHJqQpTfNwdtEnVQRZeRsHA4cRTXU0bmTemVnH8KYxgKy93yDuUMfLBw0v-STujpPx8W7iWpyyMhnqIYOOn05D1EzE85u4" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+      
+   </div>
+    
+    <div class="carousel-item">
+      
+        
+		<div class="container-xxl amit">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWOtg2CIic7X8vj9FzhWhl00Bal77UOM85vA&s" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://web.uri.edu/wp-content/uploads/sites/916/mgsl-tour-setup.jpg" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNUXzTIowBF4fGJ2GmCtEwmSU7ksBSwZYlnA&s" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+      
+      
+
+    </div>
+    <div class="carousel-item">
+      
+      	<div class="container-xxl amit">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://www.gncbudhlada.org/uploads/files/z5.jpeg" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzUjIgdliMur5A_bnk_Nuqco2GvAKotzJMMA&s" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+      <img src="https://content.jdmagicbox.com/v2/comp/kannur/w2/9999px497.x497.250128230756.f7w2/catalogue/iconic-fitness-center-family-gym-pallikunnu-kannur-fitness-centres-kve0xh2f1c-250.jpg" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+		
+      
+    </div>
+     
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+
+
+
+
+</div>
+
+<br></br><br></br><br></br>
+  
+  
+  <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     
+		<div class="container-xxl amit">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://www.collidu.com/media/catalog/product/img1/3/8/3881f1e33f04f7354c06451f09de727f7c3d6310fcc93cfa91690c4ae5a5955a/modern-college-theme-slide8.png" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://png.pngtree.com/background/20230519/original/pngtree-the-college-of-bethlehem-campus-of-mccord-university-picture-image_2668225.jpg" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+     <img src="https://png.pngtree.com/element_pic/17/08/05/605648083c9b6c687a429b63e6df888e.png" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+      
+   </div>
+    
+    <div class="carousel-item">
+      
+        
+		<div class="container-xxl amit">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWOtg2CIic7X8vj9FzhWhl00Bal77UOM85vA&s" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://web.uri.edu/wp-content/uploads/sites/916/mgsl-tour-setup.jpg" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNUXzTIowBF4fGJ2GmCtEwmSU7ksBSwZYlnA&s" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+      
+      
+
+    </div>
+    <div class="carousel-item">
+      
+      	<div class="container-xxl amit">
+		
+		<div class="row amity">
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100" >
+
+    <img src="https://www.collidu.com/media/catalog/product/img1/3/8/3881f1e33f04f7354c06451f09de727f7c3d6310fcc93cfa91690c4ae5a5955a/modern-college-theme-slide8.png" height="350px"/>
+		
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+    
+     <img src="https://png.pngtree.com/background/20230519/original/pngtree-the-college-of-bethlehem-campus-of-mccord-university-picture-image_2668225.jpg" height="350px"/>
+     
+		</div>
+		
+		</div>
+		
+		<div class="col-4">
+
+		<div class="card p-3 myCard h-100">
+
+      <img src="https://png.pngtree.com/element_pic/17/08/05/605648083c9b6c687a429b63e6df888e.png" height="350px"/>
+		
+		</div>
+		
+		</div>
+
+		
+		</div> 
+		</div>
+		
+      
+    </div>
+     
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Nexti</span>
+  </button>
+</div>
+  
+  
+  
+  
+  
+  
+  
+<br></br><br></br><br></br><br></br>
+  <br></br><br></br><br></br><br></br><br></br><br></br>  
+
+
+<img src="https://www.pngall.com/wp-content/uploads/4/Black-Border-PNG-High-Quality-Image.png" height="280px" width="1190px" />
+
+
+<br></br><br></br><br></br><br></br><br></br><br></br>
+    
+    </div>
+    </>
+    
+    
+    
+    
+    
+    )
+};
+
+
+
+
+
+export default Board;
