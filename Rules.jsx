c9b249fff407a8fda9721bff91dd746c8b2a8696
tssnainital-byte <tssnainital@gmail.com>
2026-03-24 16:54:03 +0530

    Create Rules.jsx

---
+++ Rules.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+
+const Rules = () => {
+  
+  return(
+    <>
+    
+    <div className="container" style={{ height: 'calc(100vh - 140px)', overflow: 'auto' }}>
+    
+  <nav class="navbar "  style={{color:"white" , boxShadow: '0 8px 16px rgba(0,0,0,0.3)' }}>
+  <div class="container">
+    <a class="navbar-brand" href="#" style={{ marginLeft:"410px"}} >
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-e6mTrk5rNqZqOMGJ3IbH54sio_3c4w3rXw&s" alt="Bootstrap" width="130" height="110"/>
+    </a>
+  </div>
+</nav>
+    
+    
+    
+     <div class="accordion" id="accordionExample" style={{marginTop:"4px"}}>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <b> Information</b>
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        
+        
+        <b>Information (XYZ University):</b>
+<br></br><br></br>
+
+
+Welcome from Rule Office !
+University rules and regulations maintain campus discipline, academic integrity, and safety. Mandatory requirements typically include wearing ID cards, maintaining a minimum 75% attendance, adhering to strict dress codes, and avoiding acts of ragging or misbehavior. Smoking, alcohol, and drug possession are strictly prohibited on campus. 
+        below are explain version...
+        
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+       <b>Rules & Regulations</b>
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <b>Common  Regulations:</b>
+        <br></br><br></br>
+<b>  Key Rules and Regulations</b> <br></br><br></br>
+
+<b>Attendance & Punctuality:</b> Minimum 75% attendance is usually required to appear for examinations. Regular attendance is mandatory; latecomers may not be allowed in class.<br></br>
+<b>Dress Code & ID Cards:</b> Students must wear neat, decent clothes or the prescribed uniform. Valid ID cards must be displayed at all times.<br></br>
+<b>Behavioral Conduct:</b> Dignified behavior is expected. Misbehavior towards staff, faculty, or other students is punishable.<br></br>
+<br></br><br></br>
+<b> Prohibited Items/Actions:</b>  <br></br><br></br>
+<b>Smoking/Alcohol/Drugs:</b>  Strictly prohibited on campus.<br></br>
+<b>Mobile Phones: </b> Use of phones, cameras, or unauthorized electronics is banned in classrooms and labs.<br></br>
+<b>Ragging:</b>  Strictly prohibited and punishable by law.<br></br>
+<b>Campus Discipline:</b> No loitering in corridors during class hours. Students must not damage college property, such as writing on walls or desks.<br></br>
+<b>Library Rules: </b>  Books must be returned on time. Damaged or lost books require replacement or payment. <br></br>
+<b>Vehicles:</b>  Parking is restricted to designated areas, and helmets/licenses are required.<br></br>
+<b>Functions/Meetings:</b>  No meeting or entertainment event can be organized without the Principal's prior written permission.<br></br>
+        
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+       <b> Usual Common Requirements </b>
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong> Mandatory Requirements for Students </strong> 
+
+        
